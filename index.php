ac322f86b8471148632a18b0f4b3c0a20bb8ca06
Nagima Arstanbekkyzy <75328910+bonnagima@users.noreply.github.com>
2021-05-23 20:18:55 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My portfolio</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link href="https://use.fontawesome.com/releases/v5.0.0/css/all.css" rel="stylesheet">
+</head>
+<body>
+<div class="prof-box"> 
+    <img src="images/cat.jpg" class="prof-pic">
+    <h1>Nagima Arstanbekkyzy</h1>
+    <div class="line-break"></div>
+    <h3>Student of Ala-Too International University</h3>
+    <h4>I am in my 2nd year of college</h4>
+    <h5>My group: SCA-19-A</h5>
+    <h6>My speciality: Programmer</h6>
+    <div class="line-break2"></div>
+    
+    <div class="social-icons">
+        <div>
+            <h2 style="font-size: 30px;">My contacts:</h2>
+        </div>
+        <a href="https://wa.me/996702755935" target="_blank"><i class="fab fa-whatsapp"></i></a>
+        <a href="https://t.me/bonnorii" target="_blank"><i class="fab fa-telegram"></i></a>
+        <a href="https://instagram.com/bonnorii?utm_medium=copy_link" target="_blank"><i class="fab fa-instagram"></i></a>
+    </div>
+</div>
+</body>
+</html>
